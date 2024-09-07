7a041f382b6062fd013cf04faa6a7c72f884fb6a
yunleizhang <yunleizhang@tencent.com>
2024-09-07 16:05:19 +0800

    feat: weChat page

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,117 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon, CheckIcon, BackwardIcon, VideoCameraIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
+  const list = [
+    {
+      year: 2022,
+      boyText: '我们恋爱啦🥰',
+      girlText: '我们恋爱啦🥰'
+    },
+    {
+      year: 2023,
+      boyText: '宝宝～我爱你😍',
+      girlText: '老公～我也爱你😍'
+    },
+    {
+      year: 2024,
+      boyText: '我们结婚啦😘',
+      girlText: '我们结婚啦😘'
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        {/* <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.----
-          </p> */}
-          {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/> */}
-          {/* <div className={styles.shape}/>
-          <Link
+    <main className="bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <div className='py-10 px-4 min-h-screen bg-white/30 backdrop-blur-sm'>
+        <div className='max-w-5xl mx-auto'>
+          <div className="flex item-center justify-between">
+            <div className='flex items-center gap-2 text-md'>
+              <BackwardIcon className="h-5 w-5 md:h-10 md:w-10 text-white" />
+              <Image
+                src='/soul.webp'
+                width={20}
+                height={20}
+                className='block md:hidden rounded-sm'
+                alt="soul app icon"
+              />
+              <Image
+                src='/soul.webp'
+                width={30}
+                height={30}
+                className='hidden md:block rounded-sm'
+                alt="soul app icon"
+              />
+            </div>
+            <p className='text-2xl md:text-4xl text-white font-bold'>我们俩的可爱情头</p>
+            {/* <p className='text-md text-white'>Feature❤️</p> */}
+            <VideoCameraIcon className='w-5 md:w-10 text-white' />
+          </div>
+          <p className="py-4 md:py-10 text-center text-white text-base">✨2022.08.25✨</p>
+          {
+            list.map((item) => (
+              <div className="w-full py-4" key={item.year}>
+                <div className="flex justify-center py-4 md:py-8">
+                  <p className='text-lg md:text-3xl bg-black/30 backdrop-blur-sm px-4 md:px-8 rounded-2xl text-white'>{item.year}</p>
+                </div>
+                <div className="flex items-center">
+                  <Image
+                    src={`/boy-${item.year}.jpg`}
+                    width={200}
+                    height={200}
+                    className='hidden md:block rounded-xl my-4'
+                    alt="avatar of the dashboard project showing desktop version"
+                  />
+                  <Image
+                    src={`/boy-${item.year}.jpg`}
+                    width={80}
+                    height={80}
+                    className='block md:hidden rounded-xl my-4'
+                    alt="avatar of the dashboard project showing mobile version"
+                  />
+                  <div className={`${styles.chatBubbleLeft} ml-4 p-4 min-w-24 bg-white rounded-2xl`}>
+                    <p className='min-w-24 text-xl md:text-3xl'>{item.boyText}</p>
+                  </div>
+                </div>
+                <div className='flex items-center justify-end'>
+                  <div className={`${styles.chatBubbleRight} mr-4 p-4 min-w-24 bg-green-200 rounded-2xl`}>
+                    <p className='text-xl md:text-3xl'>{item.girlText}</p>
+                    <p className='flex justify-end md:pt-4 text-md md:text-xl text-green-500'>
+                      <span className='pr-1'>5:20</span>
+                      <CheckIcon className='w-5 text-green-500' />
+                      <CheckIcon className='w-5 text-green-500 -ml-6' />
+                    </p>
+                  </div>
+                  <Image
+                    src={`/girl-${item.year}.jpg`}
+                    width={200}
+                    height={200}
+                    className='hidden md:block rounded-xl my-4'
+                    alt="avatar of the dashboard project showing desktop version"
+                  />
+                  <Image
+                    src={`/girl-${item.year}.jpg`}
+                    width={80}
+                    height={80}
+                    className='block md:hidden rounded-xl my-4'
+                    alt="avatar of the dashboard project showing mobile version"
+                  />
+                </div>
+              </div>
+            ))     
+          }
+          {/* <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center justify-center gap-5 text-sm"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div> */}
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className='hidden md:block'
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className='block md:hidden'
-            alt="Screenshots of the dashboard project showing mobile version"
-          />
+            <ArrowRightIcon className="w-5 md:w-6" />
+          </Link> */}
+          <div className="flex items-center justify-center gap-2 text-white text-sm">
+          <p className="py-10 text-center">Waiting Open</p>
+          <ArrowRightIcon className="w-5 md:w-6 text-sm" />
+          </div>
         </div>
       </div>
     </main>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -18,7 +18,7 @@ export default function LoginForm() {
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please unlock to view.
         </h1>
         <div className="w-full">
           <div>
@@ -83,7 +83,7 @@ function LoginButton() {
   const { pending } = useFormStatus();
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      Unlock <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
